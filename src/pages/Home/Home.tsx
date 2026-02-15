9208eeaff85a477f83dd65e0dd37b4988ca27ae7
Tornike Khutsishvili <tornikexucishvili0@gmail.com>
2026-02-15 04:15:08 +0400

    Add card, cardWrapper, currency components, also module css files

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
+import CardsWrapper from "../../components/CardsWrapper";
 import Carousel from "../../components/Carousel";
 
 const Home = () => {
   return (
     <>
       <Carousel />
+      <CardsWrapper />
     </>
   );
 };
